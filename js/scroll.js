8375197f82b8c22b41444fb8557fe3d2c6f7998c
mavidunya <mrvksy@outlook.com>
2017-10-22 23:51:48 +0300

    draft

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,26 @@
-$(document).ready(function() {
-  
+/**
+ * MENU SCROLL
+ */
+
+$(document).ready(function () {
+
   var scrollLink = $('.scroll');
-  
+
   // Smooth scrolling
-  scrollLink.click(function(e) {
+  scrollLink.click(function (e) {
     e.preventDefault();
     $('body,html').animate({
       scrollTop: $(this.hash).offset().top
-    }, 1000 );
+    }, 1000);
   });
+})
+
+/**
+ * RESPONSİVE DROPDOWN MENU
+ */
+
+$(function () {
+  $(".menu-btn").click(function () {
+    $(".menu").slideToggle(500);
+  })
 })
